879be7038ddae4e237ca10887a3bb67ad946d855
Wenkai Li <kaien125@hotmail.com>
2023-06-14 20:32:28 +1000

    add results for diff data

--- run_eval_for_different_data.sh
+++ run_eval_for_different_data.sh
@@ -4,7 +4,7 @@ NUM_EPOCHS=10
 RESIZE_SCALE=0.5
 NUM_IMAGE=10000
 train_image_size=224
-train_contrast_reduce="1-20"
+train_contrast_reduce="121-140"
 
 
 for CONTRAST_REDUCE in "1-20" "21-40" "41-60" "61-80" "81-100" "101-120" "121-140" "141-160" "161-180" "181-200"
@@ -17,7 +17,7 @@ do
       # evaluate
       for SUB_PATH in "learning_test" "part_whole_test" "part_whole_flip_test" "learning_occ_test" "one_test" "part_whole_occ_test" "part_whole_flip_occ_test" 
       do
-         EVAL_FILE_NAME="eval_${MODEL}_bs${BS}_e${NUM_EPOCHS}_trainIs${train_image_size}_trainContrastReduce${train_contrast_reduce}_testIs${test_image_size}_i${NUM_IMAGE}_test${IMAGE_PATH}.txt"
+         EVAL_FILE_NAME="eval_${MODEL}_bs${BS}_e${NUM_EPOCHS}_trainIs${train_image_size}_trainContrastReduce${train_contrast_reduce}\eval_${MODEL}_bs${BS}_e${NUM_EPOCHS}_trainIs${train_image_size}_trainContrastReduce${train_contrast_reduce}_testIs${test_image_size}_i${NUM_IMAGE}_test${IMAGE_PATH}.txt"
          python eval_for_different_data.py \
          --model $MODEL \
          --test_image_size $test_image_size \
